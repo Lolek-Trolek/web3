54b1a3f259cd5ac74decfe0c711e21efdde34025
Brzezowski Jakub <jakub.brzezowski@student.vinci.be>
2024-12-15 15:57:27 +0100

    1.1-1.2 : refactor - modules

--- exercices/module1/introdemo/src/App.jsx
+++ exercices/module1/introdemo/src/App.jsx
@@ -1,32 +1,6 @@
-const Header = ({course}) => {
-  return (  
-    <h1>{course}</h1>
-  );
-};
-const Content = ({part1, exercises1,part2, exercises2,part3, exercises3}) => {
-  return (
-    <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
-    </div>
-  );
-};
-const Total = ({exercises1, exercises2, exercises3}) => {
-  return (
-    <p>
-      Number of exercises {exercises1 + exercises2 + exercises3}
-    </p>
-  );
-};
-const Part = ({part,exercises}) => {  
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-}
-
+import Header from "./components/Header/header";
+import Content from "./components/Content/content";
+import Total from "./components/Total/total";
 
 const App = () => {
   const course = "Half Stack application development";
