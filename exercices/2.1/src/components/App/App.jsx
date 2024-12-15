bcc6c84ff717a73c12ab1521afe4ed4b45f80f08
Brzezowski Jakub <jakub.brzezowski@student.vinci.be>
2024-12-15 23:48:40 +0100

    new:ex2.1-destructuring

--- exercices/2.1/src/components/App/App.jsx
+++ exercices/2.1/src/components/App/App.jsx
@@ -4,7 +4,8 @@ import viteLogo from '../../../public/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const title = "Click Counter";
+  const message = "You are a master in the art of clicking !";
   return (
     <>
       <div>
@@ -17,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Counter/>
+        <Counter title={title} message={message} />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- exercices/2.1/src/components/Compteur/ClickCounter.jsx
+++ exercices/2.1/src/components/Compteur/ClickCounter.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
-const Counter = () => {
+const Counter = ({title, message}) => {
   const [count, setCount] = useState(0);
   const handleClick = () => setCount(count + 1);
   return (
+    <>
+    <h1>{title}</h1>
     <button onClick={handleClick}>
       {count}
     </button>
+    <p>{count >= 10 && message}</p>
+    </>
   );
 };
 
