731597f9865bf9e421e7fb58ec39fbe8b7cc159d
Brzezowski Jakub <jakub.brzezowski@student.vinci.be>
2024-12-16 21:48:16 +0100

    feature 2.5 done

--- exercices/module3/2.1-2.5/src/components/Course/course.jsx
+++ exercices/module3/2.1-2.5/src/components/Course/course.jsx
@@ -2,12 +2,20 @@ import Header from '../Header/header';
 import Content from '../Content/content';
 import '../Content/content.css';
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
+  
   return (
-    <div>
+    courses.map(course =>{
+      const total = course.parts.reduce((sum,part)=>sum+part.exercises,0);
+      return (
+      <div key={course.id}>
       <Header name={course.name} />
-      <Content className ='app-container' parts={course.parts} />
-    </div>
+      <Content className ='app-container' parts={course.parts}/>
+      <p><strong>Total of {total} exercises</strong></p>
+      </div>
+      )
+    }
+    )
   );
 };
 
