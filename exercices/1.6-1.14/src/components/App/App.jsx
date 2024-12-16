6be732ed078f787ac35e09683d14009a5da69545
Brzezowski Jakub <jakub.brzezowski@student.vinci.be>
2024-12-16 18:53:31 +0100

    Loading features

--- exercices/1.6-1.14/src/components/App/App.jsx
+++ exercices/1.6-1.14/src/components/App/App.jsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import Bouton from '../Bouton/Bouton'
 import Stats from '../Stats/stats'
+import Loading from '../Loading/loading'
 import './App.css'
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [loading,setLoading] = useState(true);
+    
+    useEffect(() => {
+        
+        const timer =setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(timer)
+    }, []);
+
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Bouton onClick={()=>setGood(good+1)} text={"good"}/>
-      <Bouton onClick={()=>setNeutral(neutral+1)} text={"neutral"}/>
-      <Bouton onClick={()=>setBad(bad+1)} text={"bad"}/>
-      <Stats good={good} neutral={neutral} bad={bad}/>
+      {loading ?
+        <Loading/> :
+        (<div>
+          <h1>give feedback</h1>
+          <Bouton onClick={()=>setGood(good+1)} text={"good"}/>
+          <Bouton onClick={()=>setNeutral(neutral+1)} text={"neutral"}/>
+          <Bouton onClick={()=>setBad(bad+1)} text={"bad"}/>
+          <Stats good={good} neutral={neutral} bad={bad}/>
+        </div>
+        )
+      }
     </div>
   )
 }
---
+++ exercices/1.6-1.14/src/components/Loading/loading.jsx
@@ -0,0 +1,12 @@
+
+const Loading = () => {
+    return (
+        <div className="loading">
+        <div className="loading__spinner">
+            <p> NO i hujnia</p>
+        </div>
+        </div>
+    );
+};
+
+export default Loading;
