a254a11ee89fa14f888e3f72ca975c1476a1a44b
Brzezowski Jakub <jakub.brzezowski@student.vinci.be>
2024-12-18 12:19:27 +0100

    Exercice 2.6  done

---
+++ exercices/module4/2.6-2.10/src/components/App/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import Numbers from 'components/Numbers/numbers'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => { 
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <div>debug: {newName}</div>
+      <Numbers persons={persons} />
+    </div>
+  )
+}
+
+export default App
---
+++ exercices/module4/2.6-2.10/src/components/Numbers/numbers.jsx
@@ -0,0 +1,12 @@
+const Numbers = ({ persons }) => {
+    return (
+        <div>
+        <h2>Numbers</h2>
+        {persons.map(person => 
+            <p key={person.name}>{person.name}</p>
+        )}
+        </div>
+    );
+};
+
+export default Numbers;
